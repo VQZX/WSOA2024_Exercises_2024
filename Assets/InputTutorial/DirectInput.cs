d06e0d86e5b5a1d9837627c7a770f0972b6f05dd
Tim Flusk <tpflusk@gmail.com>
2024-03-11 14:10:54 +0200

    Separated Input and Movement

--- Assets/InputTutorial/DirectInput.cs
+++ Assets/InputTutorial/DirectInput.cs
@@ -6,6 +6,7 @@ using InputTutorial;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 using Quaternion = UnityEngine.Quaternion;
 using Vector3 = UnityEngine.Vector3;
 
@@ -20,64 +21,36 @@ public class DirectInput : MonoBehaviour
     
     private Keyboard currentKeyboard = Keyboard.current;
     private Mouse currentMouse = Mouse.current;
-    
+
+    private void Update()
+    {
+        if (CanTranslate())
+        {
+            ManageTranslation();
+        }
+
+        if (currentMouse.delta.magnitude > mouseDeadZone)
+        {
+            ManageOrientation(currentMouse.delta);
+        }
+    }
+
+    private bool CanTranslate()
+    {
+        var output = currentKeyboard.wKey.isPressed ||
+                     currentKeyboard.sKey.isPressed ||
+                     currentKeyboard.aKey.isPressed ||
+                     currentKeyboard.dKey.isPressed;
+        return output;
+    }
+
     private void ManageTranslation()
     {
         movementControl.ManageTranslation(currentKeyboard);
-        var motion = Vector3.zero;
-        if (currentKeyboard.wKey.isPressed)
-        {
-            motion += Vector3.forward;
-        }
-
-        if (currentKeyboard.sKey.isPressed)
-        {
-            motion += Vector3.back;
-        }
-
-        if (currentKeyboard.aKey.isPressed)
-        {
-            motion += Vector3.left;
-        }
-
-        if (currentKeyboard.dKey.isPressed)
-        {
-            motion += Vector3.right;
-        }
-
-        motion = motion.normalized;
-
-        var motionVector = motion * speed * Time.deltaTime;
-        
-        /*
-         * Rotation in 3D around the y-component to get the correct forward orientation
-         * Because they y-component goes upwards in Unity,
-         * We only have to worry about adjust the x and z values.
-         * As such we only rotate around the y-component in 3D as such
-         * per vector-matrix calculation
-         * x = motionVector.x * cos(theta) + 0 + motionVector.z * sin(theta)
-         * y = 0 + motionVector.y + 0
-         * z = motionVector.x * (-sin(theta)) + 0 + motionVector.z * cos(theta)
-         *
-         * Where theta is the targets y-rotation in the euler angles
-         */
-
-        var yRotation = transform.localRotation.eulerAngles.y * Mathf.Deg2Rad;
-        var xPrime = motionVector.x * Mathf.Cos(yRotation) + motionVector.z * Mathf.Sin(yRotation);
-        var zPrime = -motionVector.x * Mathf.Sin(yRotation) + motionVector.z * Mathf.Cos(yRotation);
-
-        var rotatedMotionVector = new Vector3(xPrime, motionVector.y, zPrime);
-        
-        transform.position += rotatedMotionVector * speed * Time.deltaTime;
     }
     
-    private void ManageOrientation()
+    private void ManageOrientation(DeltaControl mouseDelta)
     {
-        var mouseDelta = currentMouse.delta;
-        if (mouseDelta.magnitude < mouseDeadZone)
-        {
-            return;
-        }
         movementControl.ManageOrientation(mouseDelta);
     }
 }
